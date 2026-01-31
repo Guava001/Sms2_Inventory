52bb67dfc46cfba44560d3cf0640f6a301a909ec
Guava001 <166852728+Guava001@users.noreply.github.com>
2026-01-31 11:24:55 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -6,20 +6,20 @@ DB_USER = "root"
 DB_PASSWORD = ""
 DB_HOST = "localhost"
 DB_NAME = "sms_inventory"
-DB_PATH = os.path.join("/data", "inventory.db")
-
-engine = create_engine(
-    f"sqlite:///{DB_PATH}",
-    connect_args={"check_same_thread": False},
-    echo=False
-)
-
-SessionLocal = sessionmaker(bind=engine)
-
+# DB_PATH = os.path.join("/data", "inventory.db")
 
 # engine = create_engine(
-#     f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?charset=utf8mb4",
+#     f"sqlite:///{DB_PATH}",
+#     connect_args={"check_same_thread": False},
 #     echo=False
 # )
 
 # SessionLocal = sessionmaker(bind=engine)
+
+
+engine = create_engine(
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?charset=utf8mb4",
+    echo=False
+)
+
+SessionLocal = sessionmaker(bind=engine)
